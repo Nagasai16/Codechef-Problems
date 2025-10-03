f5ba0bed65f09872b9bb3ea6960e3fbb7685fd14
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2025-10-03 10:57:03 +0530

    Create multi.java

---
+++ Basic/Multi Variable Declaration/multi.java
@@ -0,0 +1,11 @@
+class CodeChef {
+  public Static void main(String[] args){
+    int a=3,b=2,c=5;
+    double d=8.5, e=7.9;
+    System.out.println("Goals Scored: "+a);
+    System.out.println("Assists Made: "+b);
+    System.out.println("Shots on Target: "+c);
+    System.out.println("Player 1 Rating: "+d);
+    System.out.println("Player 2 Rating: "+e);
+  }
+}
